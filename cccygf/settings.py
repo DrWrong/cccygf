6718df25da11493b37586af1e0c1a05525cd5d72
DrWrong <chengyuhang@bit.edu.cn>
2014-02-09 10:28:43 +0800

    add app staticfile

--- cccygf/settings.py
+++ cccygf/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'captcha'
+    'captcha',
+    'django.contrib.staticfiles',
 )
 
 
